18e12c7680b7b8ae1ddcb37aec3903b8d3f32c89
Matthew Barber <quitesimplymatt@gmail.com>
2020-04-05 18:42:16 +0100

    get stock description

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
 from pyhive import hive
+from os import getenv
 
-connection = hive.Connection(host='hive-server')
+def main():
+    print("Connecting to Hive")
+    connection = hive.Connection(host='hive-server')
 
-cursor = connection.cursor()
-cursor.execute('SELECT * FROM stocks LIMIT 10')
+    cursor = connection.cursor()
 
-print(cursor.fetchall())
+    symbol = getenv('SPARK_APPLICATION_ARGS')
+    cursor.execute('SELECT name FROM symbol_descriptions WHERE symbol="%s"' % symbol)
+    name = cursor.fetchall()[0]
+    print("Finding history of stock %s (%s)" % (symbol, name))
+
+if __name__ == "__main__":
+    print("Starting application")
+    main()
