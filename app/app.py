b69d6b0d85db6541e02d6bb003bd9585fb3d3464
Matthew Barber <quitesimplymatt@gmail.com>
2020-04-06 10:39:46 +0100

    use hive and spark stuff globally

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from os import getenv
 
 from pyhive import hive
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
-def get_name(conn, symbol):
-    cursor = conn.cursor()
+def get_name(symbol):
+    cursor = connection.cursor()
     cursor.execute('SELECT name FROM symbol_descriptions WHERE symbol="%s"' % symbol)
     name = cursor.fetchone()[0] # first index of a 1-element tuple
 
@@ -13,12 +15,18 @@ def main():
     symbol = getenv('SPARK_APPLICATION_ARGS')
     print("Querying symbol %s" % symbol)
 
-    print("Connecting to Hive")
-    conn = hive.Connection(host='hive-server')
 
-    name = get_name(conn, symbol)
+    name = get_name(symbol)
     print("Finding stock history of %s (%s)" % (name, symbol))
 
 if __name__ == "__main__":
     print("Starting application")
+
+    print("Connecting to Hive")
+    connection = hive.Connection(host='hive-server')
+
+    print("Connecting to Spark")
+    sc = SparkContext.getOrCreate()
+    spark = SparkSession(sc)
+
     main()
