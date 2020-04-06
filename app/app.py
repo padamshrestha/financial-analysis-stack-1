3c9c065a3f1ee8b2340285c4407f6685f5fc1d51
Matthew Barber <quitesimplymatt@gmail.com>
2020-04-06 16:12:51 +0100

    ordinal date conversions

--- app/app.py
+++ app/app.py
@@ -1,16 +1,35 @@
 from os import getenv
+from datetime import date
+import re
 
 from pyhive import hive
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import udf
+from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import LinearRegression
+
+@udf(IntegerType())
+def date_string_to_ordinal(date_str):
+    date_ = date.fromisoformat(date_str)
+    ordinal = date_.toordinal()
+    return ordinal
 
 def make_lr_model(symbol):
     cursor = connection.cursor()
     cursor.execute('SELECT date_, adjclose FROM stocks WHERE symbol="%s" LIMIT 20' % symbol)
     stock_history = cursor.fetchall()
 
-    df = spark.createDataFrame(stock_history)
+    initial_schema = StructType([
+        StructField('date', StringType()),
+        StructField('adjclose', FloatType())
+    ])
+    df = spark.createDataFrame(stock_history, schema=initial_schema)
+
+    ordinal_dates = date_string_to_ordinal(df.date)
+    df = df.withColumn('date', ordinal_dates)
+
     df.show(5)
 
 def get_name(symbol):
