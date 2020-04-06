2a9e8377a68fb7970cf1765770363846a1ebc370
Matthew Barber <quitesimplymatt@gmail.com>
2020-04-06 22:48:19 +0100

    insert into groups of starting character

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -43,9 +43,58 @@ CREATE TABLE stocks (
      PARTITIONED BY (symbol VARCHAR(5))
      STORED AS ORC;
 
-INSERT INTO TABLE stocks
-    PARTITION (symbol)
-    SELECT * FROM stocks_staging;
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "A";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "B";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "C";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "D";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "E";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "F";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "G";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "H";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "I";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "J";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "K";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "L";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "M";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "N";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "O";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "P";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "Q";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "R";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "S";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "T";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "U";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "V";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "W";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "X";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "Y";
+INSERT INTO TABLE stocks PARTITION (symbol) SELECT * FROM stocks_staging
+    WHERE SUBSTR(symbol, 0, 1) = "Z";
 
 -- 4. Delete now unnecessary staging table
 
