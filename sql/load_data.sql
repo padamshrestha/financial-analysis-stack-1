ee12d4a916504d025ff1c5b3992e9d6da8017160
Matthew Barber <quitesimplymatt@gmail.com>
2020-04-06 22:04:14 +0100

    sort dynamic partitions for huge optimisation increase

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -1,7 +1,17 @@
+-- 0. Enable dynamic partition inserts and make optimisations
+
+SET hive.exec.dynamic.partition.mode=nonstrict;
+SET hive.optimize.sort.dynamic.partition=true;
+
+-- 1. Drop tables if they already exist
+
+DROP TABLE IF EXISTS stocks_staging;
 DROP TABLE IF EXISTS stocks;
 DROP TABLE IF EXISTS symbol_descriptions;
 
-CREATE EXTERNAL TABLE IF NOT EXISTS stocks (
+-- 2. Use staging tables to temporarily load the data
+
+CREATE EXTERNAL TABLE stocks_staging (
   symbol VARCHAR(5),
   date_ DATE,
   volume INT,
@@ -17,7 +27,31 @@ CREATE EXTERNAL TABLE IF NOT EXISTS stocks (
     TBLPROPERTIES ("skip.header.line.count"="1");
 
 LOAD DATA LOCAL INPATH '/data/stock_histories.csv'
-    OVERWRITE INTO TABLE stocks;
+    OVERWRITE INTO TABLE stocks_staging;
+
+-- 3. Insert staged table into an optimised table
+
+CREATE TABLE stocks (
+   date_ DATE,
+   volume INT,
+   open FLOAT,
+   close FLOAT,
+   high FLOAT,
+   low FLOAT,
+   adjclose FLOAT
+ )
+     PARTITIONED BY (symbol)
+     STORED AS ORC;
+
+INSERT INTO TABLE stocks
+    PARTITION (symbol)
+    SELECT * FROM stocks_staging;
+
+-- 4. Delete now unnecessary staging table
+
+DROP TABLE stocks_staging;
+
+-- 5. Load in the symbol descriptions
 
 CREATE TABLE IF NOT EXISTS symbol_descriptions (
   symbol VARCHAR(5),
