4c4453353a6a56fc29e90af2bcede7abdfb723ae
Josh <joshgallagher24@gmail.com>
2020-04-07 17:35:00 +0100

    Established Redis connection and setup stock name caching

--- app/app.py
+++ app/app.py
@@ -2,7 +2,9 @@ from argparse import ArgumentParser
 from os import getenv
 from datetime import date
 import re
+import pickle
 
+import redis
 from pyhive import hive
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
@@ -12,16 +14,24 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import LinearRegression
 
 # Assign these global vars at runtime
+r = None
 connection = None
 sc = None
 spark = None
 
-def get_name(symbol):
-    cursor = connection.cursor()
-    cursor.execute('SELECT name FROM symbol_descriptions WHERE symbol="%s"' % symbol)
-    name = cursor.fetchone()[0] # first index of a 1-element tuple
 
+def get_name(symbol):
+    if not r.exists('symbol-' + symbol):
+        cursor = connection.cursor()
+        cursor.execute(
+            'SELECT name FROM symbol_descriptions WHERE symbol="%s"' % symbol)
+        name = cursor.fetchone()[0]  # first index of a 1-element tuple
+        r.set('symbol-' + symbol, name)
+        return name
+
+    name = r.get('symbol-' + symbol)
     return name
+
 
 @udf(IntegerType())
 def date_string_to_ordinal(date_str):
@@ -30,9 +40,11 @@ def date_string_to_ordinal(date_str):
 
     return ordinal
 
+
 def make_lr_model(symbol):
     cursor = connection.cursor()
-    cursor.execute('SELECT date_, close FROM stocks WHERE symbol="%s" LIMIT 20' % symbol)
+    cursor.execute(
+        'SELECT date_, close FROM stocks WHERE symbol="%s" LIMIT 20' % symbol)
     stock_history = cursor.fetchall()
 
     initial_schema = StructType([
@@ -53,21 +65,23 @@ def make_lr_model(symbol):
     return lr_model
 
 def predict_close(lr_model, date_ordinal):
-    ordinal_schema = StructType([ StructField('date', IntegerType()) ] )
+    ordinal_schema = StructType([StructField('date', IntegerType())])
     df = spark.createDataFrame([date_ordinal], schema=ordinal_schema)
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('symbol',
-                        help = "Stock listing to model for")
+                        help="Stock listing to model for")
     parser.add_argument('date',
-                        type = date.fromisoformat,
-                        help = "Predict stock's price at <yyyy-mm-dd>")
+                        type=date.fromisoformat,
+                        help="Predict stock's price at <yyyy-mm-dd>")
     args_env = getenv('SPARK_APPLICATION_ARGS').split(sep=' ')
     args = parser.parse_args(args_env)
 
-    global connection, sc, spark
+    global r, connection, sc, spark
+    print("Connecting to Redis")
+    r = redis.Redis(host='redis', db=0)
     print("Connecting to Hive")
     connection = hive.Connection(host='hive-server')
     print("Connecting to Spark")
@@ -88,6 +102,7 @@ def main():
     close = predict_close(lr_model, args.date.toordinal())
     print("Estimated price of %s at %s: $%s"
           % (symbol, date.isoformat(), '$'+format(close, ',.2f')))
+
 
 if __name__ == "__main__":
     print("Starting application")
