23c6588b4a4551bd5a3df28af280c37ece64e41f
Matthew Barber <quitesimplymatt@gmail.com>
2020-04-09 14:13:26 +0100

    hive_setup.sql clean up (#19)
    * remove unnecessary if not exists statements
    
    * remove first row ignore
    
    The first row is data, not a header!

--- hive/hive_setup.sql
+++ hive/hive_setup.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS stocks;
 DROP TABLE IF EXISTS symbol_descriptions;
 
-CREATE EXTERNAL TABLE IF NOT EXISTS stocks (
+CREATE EXTERNAL TABLE stocks (
   symbol VARCHAR(5),
   date_ DATE,
   volume INT,
@@ -13,13 +13,12 @@ CREATE EXTERNAL TABLE IF NOT EXISTS stocks (
 )
     ROW FORMAT DELIMITED
     FIELDS TERMINATED BY ','
-    STORED AS TEXTFILE
-    TBLPROPERTIES ("skip.header.line.count"="1");
+    STORED AS TEXTFILE;
 
 LOAD DATA LOCAL INPATH '/data/stock_histories.csv.gz'
     OVERWRITE INTO TABLE stocks;
 
-CREATE TABLE IF NOT EXISTS symbol_descriptions (
+CREATE TABLE symbol_descriptions (
   symbol VARCHAR(5),
   name STRING
 )
